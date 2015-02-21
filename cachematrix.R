fa5c6c76972fc21c12300a9971c7c6b7627ed96e
sarita tellez <saritalt90@hotmail.com>
2015-02-21 12:15:34 -0800

    edit cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,5 +1,6 @@
-## Put comments here that give an overall description of what your
-## functions do
+# The purpose of the assignment is to create two functions. One that creates a matrix
+# whose inverse can be cached, and second one that determines the inverse matrix or checks
+# if the inverse has been cached. 
 
 ## This function creates a matrix whose inverse can be cached
 
@@ -24,11 +25,11 @@ makeCacheMatrix <- function(x = matrix()) {
 }
 
 
-## Write a short comment describing this function
-## Return a matrix that is the inverse of 'x'
+## This function checks if a matrices inverse has been cached and returns it.
+## If it hasn't been cached, it determines the inverse of the matrix "x" 
 cacheSolve <- function(x, ...) {
   
-  
+  #see if inverse matrix has been cached 
   m <- x$getInverseMatrix ()
     if(!is.null(m)) {
       message("getting cached data")
